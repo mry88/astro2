7b7c0af7f6019e5ef97670673d30917097e444dc
mry88 <yusufmuhammadrizal@gmail.com>
2023-08-21 11:25:27 +0700

    3.1

---
+++ frontend/src/components/NavBar.jsx
@@ -0,0 +1,85 @@
+import { Link } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import styled from "styled-components";
+import { logoutUser } from "../slices/authSlice";
+import { toast } from "react-toastify";
+import {faCartPlus} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import "../App.css";
+
+
+const NavBar = () => {
+  const dispatch = useDispatch();
+  const { cartTotalQuantity } = useSelector((state) => state.cart);
+  const auth = useSelector((state) => state.auth);
+
+  return (
+    <nav className="nav-bar">
+      <Link to="/">
+        <h2>AstroFLaz</h2>
+      </Link>
+      <Link to="/cart">
+        <div className="nav-bag">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="35"
+            height="35"
+            fill="currentColor"
+            className="bi bi-handbag-fill"
+            viewBox="0 0 16 16"
+          >
+            <FontAwesomeIcon icon={faCartPlus}></FontAwesomeIcon>
+          </svg>
+          <span className="bag-quantity">
+            <span>{cartTotalQuantity}</span>
+          </span>
+        </div>
+      </Link>
+      {auth._id ? (
+        <Links>
+          {auth.isAdmin ? (
+            <div>
+              <Link to="/admin/summary">Admin</Link>
+            </div>
+          ) : null}
+          <div
+            onClick={() => {
+              dispatch(logoutUser(null));
+              toast.warning("Logged out!", { position: "bottom-left" });
+            }}
+          >
+            Logout
+          </div>
+        </Links>
+      ) : (
+        <AuthLinks>
+          <Link to="/login">Login</Link>
+          <Link to="register">Register</Link>
+        </AuthLinks>
+      )}
+    </nav>
+  );
+};
+
+export default NavBar;
+
+const AuthLinks = styled.div`
+  a {
+    &:last-child {
+      margin-left: 2rem;
+    }
+  }
+`;
+
+const Links = styled.div`
+  color: white;
+  display: flex;
+
+  div {
+    cursor: pointer;
+
+    &:last-child {
+      margin-left: 2rem;
+    }
+  }
+`;
