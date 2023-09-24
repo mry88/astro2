3ff2951a5fafcd4ed872e683d9c055c113f81a57
Jaya240801 <hanjayaputra08@gmail.com>
2023-09-25 00:55:47 +0700

    Menata struktur PDF order detail

--- frontend/src/components/OrderPDF.jsx
+++ frontend/src/components/OrderPDF.jsx
@@ -11,30 +11,40 @@ const OrderPDF = ({ orderData, orderQuantity, orderTotal }) => {
       <Document>
         <Page size="A4">
           <View style={styles.container}>
+            <View style={styles.header}>
+              <Text style={styles.companyName}>Code Space Indoneisa</Text>
+              <Text style={styles.companyAddress}>Kota Malang, Jawa Timur</Text>
+            </View>
+
             <Text style={styles.title}>Order Details</Text>
 
             {cart.cartItems &&
               cart.cartItems.map((cartItem) => (
                 <div>
                   <View style={styles.section}>
-                    <Text style={styles.subtitle}>Customer Information:</Text>
-                    <Text>Name: {auth.name}</Text>
-                    <Text>Email: {auth.email}</Text>
+                    <Text style={styles.subtitle}>Customer Information :</Text>
+                    <Text>Name  : {auth.name}</Text>
+                    <Text>Email : {auth.email}</Text>
                   </View>
 
                   <View style={styles.section}>
-                    <Text style={styles.subtitle}>Order Items:</Text>
+                    <Text style={styles.subtitle}>Order Items :</Text>
                     <View key={cartItem._id} style={styles.item}>
-                      <Text>Product: {cartItem.name}</Text>
-                      <Text>Price: Rp.{cartItem.totalPrice}</Text>
-                      <Text>Quantity: {cartItem.cartQuantity}</Text>
+                      <Text>Product  : {cartItem.name}</Text>
+                      <Text>Price    : Rp.{cartItem.totalPrice}</Text>
+                      <Text>Quantity : {cartItem.cartQuantity}</Text>
                     </View>
                   </View>
 
                   <View style={styles.section}>
-                    <Text style={styles.subtitle}>Order Summary:</Text>
-                    <Text>Quantity: {cart.cartTotalQuantity}</Text>
-                    <Text>Total: Rp.{cart.cartTotalAmount}</Text>
+                    <Text style={styles.subtitle}>Order Summary :</Text>
+                    <Text>Quantity : {cart.cartTotalQuantity}</Text>
+                    <Text>Total    : Rp.{cart.cartTotalAmount}</Text>
+                  </View>
+
+                  <View style={styles.footer}>
+                    <Text style={styles.gratitude}>Thank you for your order!</Text>
+                    <Text style={styles.gratitude}>Check your email for more information!</Text>
                   </View>
                 </div>
               ))
@@ -48,26 +58,58 @@ const OrderPDF = ({ orderData, orderQuantity, orderTotal }) => {
 
 const styles = StyleSheet.create({
   pdfViewer: {
-    height: 500,
-    width: 500,
+    height: 500, 
+    width: 500,  
     margin: "auto",
   },
   container: {
     padding: 20,
+    fontFamily: "Courier",
+  },
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginBottom: 20,
+  },
+  companyName: {
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  companyAddress: {
+    fontSize: 12,
   },
   title: {
-    fontSize: 24,
-    marginBottom: 10,
+    fontSize: 26,
+    marginBottom: 20,
+    borderBottom: 2,
+    paddingBottom: 10,
+    fontFamily: "Courier",
   },
   section: {
     marginBottom: 20,
   },
   subtitle: {
-    fontSize: 18,
-    marginBottom: 10,
+    fontSize: 20,
+    marginBottom: 15,
+    fontFamily: "Courier",
+  },
+  orderItem: {
+    marginBottom: 30,
   },
   item: {
-    marginBottom: 10,
+    marginBottom: 15,
+    border: 1,
+    padding: 10,
+  },
+  footer: {
+    position: "center",
+    marginTop: 30,
+    textAlign: "center",
+    fontFamily: "Courier",
+  },
+  gratitude: {
+    fontSize: 18,
+    marginTop: 10,
   },
 });
 
